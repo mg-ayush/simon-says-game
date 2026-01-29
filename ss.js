8577f6d3a911e0d26aa2384da7c9f13ae2fc2480
Ayush Mittal <mittalayush217@gmail.com>
2026-01-30 00:49:41 +0530

    Fixed 2nd Button not Blinking Problem

--- ss.js
+++ ss.js
@@ -39,7 +39,7 @@ function levelUp() {
     level++;
     h2.innerText = `level ${level}`;
 
-    let randIdx = Math.floor(Math.random() *3);
+    let randIdx = Math.floor(Math.random() *4);
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
     // console.log(randIdx);
